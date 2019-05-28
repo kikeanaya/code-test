866d3b41aaa520ffa2b33a321038514cf45d27a7
kikeanaya <luiseanayam@gmail.com>
2019-05-28 18:23:49 +0200

    Updates

---
+++ precache-manifest.441ee8b3ac1d09faa6feb3bfd42439e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4f0d7fc7669d8fc19d7503650519b4",
+    "url": "/code-test/index.html"
+  },
+  {
+    "revision": "1dc09f796b9c363cb17e",
+    "url": "/code-test/static/css/main.1ab20d85.chunk.css"
+  },
+  {
+    "revision": "53b1639f977ad8e98e38",
+    "url": "/code-test/static/js/2.c8cc946c.chunk.js"
+  },
+  {
+    "revision": "1dc09f796b9c363cb17e",
+    "url": "/code-test/static/js/main.9708bea8.chunk.js"
+  },
+  {
+    "revision": "b92a816a7bd271f0312b",
+    "url": "/code-test/static/js/runtime~main.c9eb859d.js"
+  }
+]);
